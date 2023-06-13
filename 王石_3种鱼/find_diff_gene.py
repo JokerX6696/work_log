df4121e35e3d47f98cabcc47ff1a662d60ea2784
JokerX6696 <313445042@qq.com>
2023-06-13 09:33:52 +0800

    解决基因多个依靠逗号分割问题

--- 王石_3种鱼/find_diff_gene.py
+++ 王石_3种鱼/find_diff_gene.py
@@ -9,15 +9,21 @@ def stat(t,h,l):
     temp_df = df[df[h] != df[l]]
     all_list = last_name + samples + next_name
     temp_df = temp_df[all_list]
-    outname = h + '_' + l + '_' + t + '_diff.xls'
-    outlist = h + '_' + l + '_' + t +'_diff_genelist.xls'
+    outname = h + '_' + l + '_' + t.replace('annotation.xls','') + '_diff.xls'
+    outlist = h + '_' + l + '_' + t.replace('annotation.xls','') +'_diff_genelist.xls'
     out = open(outlist, 'w')
     temp_df.to_csv(outname, index=False)
     gene_list_temp = temp_df['Gene.refGene'].to_list()
     # 修改基因名称
     pattern = 'gene-'
     gene_list = [i.replace(pattern,'') for i in list(set(gene_list_temp)) ]
-    for i in gene_list:
+    g_list = []
+    for gene in gene_list:
+        split_gene = gene.split(',')
+        for b in split_gene:
+            if b not in g_list:
+                g_list.append(b)
+    for i in g_list:
         print(i,file=out)
     out.close
 
