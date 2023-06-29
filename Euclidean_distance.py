484e50d920088c7efc6ecad03a09e4ae88bba22c
JokerX6696 <313445042@qq.com>
2023-06-29 15:46:12 +0800

    欧式距离计算，待完成

---
+++ Euclidean_distance.py
@@ -0,0 +1,17 @@
+from math import sqrt
+lim = 2
+x = []
+y = []
+
+start = 0
+while start < lim:
+    x.append(3)
+    y.append(4)
+    start += 1
+
+
+
+cd = len(x)
+
+far = sqrt(x[0]**2 + y[0]**2)
+print(far)
